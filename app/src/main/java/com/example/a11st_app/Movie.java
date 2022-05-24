45056a5618c91751206986ca20ed1a4f53aad400
yoonanet <asas7715@naver.com>
2022-05-24 16:15:59 +0900

    영화정보의수 추가

---
+++ app/src/main/java/com/example/a11st_app/Movie.java
@@ -0,0 +1,22 @@
+package com.example.a11st_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String moviCd;
+    String movieNm;
+    String openDt;
+    String salesAnt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiInten;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
